cb4770396d9819e477468d612619bf8999ffade0
LennoxCode <leonard.valentin1999@gmail.com>
2022-04-22 20:01:05 +0200

    implemented first working solution

--- Ueb1/src/s0575695/Main.java
+++ Ueb1/src/s0575695/Main.java
@@ -5,7 +5,7 @@ import lenz.htw.ai4g.Simulator;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("hello world");
-		String[] simArgs = {"0 xCQIkcp9ZB UfLdaNN68g"};
+		String[] simArgs = {"0 xCQIkcp9ZB UfLdaNN68g", "0 I0RxQhBTuW gEJTDkqrJJ", "0 PL7J4hdJBy"};
 		Simulator.main(simArgs);
 	}
 }
